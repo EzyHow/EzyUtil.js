392b6e6a9c3f58fcf43e3e4109ad79a89c234a44
Harinder Singh <harindergodhra@gmail.com>
2020-12-17 23:02:03 +0530

    add webpack | version 1.0.4

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [/node_modules/, /lib/],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'ezyutil.js',
+        path: path.resolve(__dirname),
+        libraryTarget: 'var',
+        library: 'ezyutil'
+    },
+};
